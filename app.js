9fa784ec8cca89afd3b932e491e07113f7bd7b00
Adam Stachowicz <saibamenppl@gmail.com>
2020-10-03 18:55:44 +0000

    Fix comments readable in app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-//fetch latest version
+// Fetch latest version
 fetch("https://vanced.app/api/v1/vanced.json")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("vanced-version").innerText = JSON.parse(data).version;
   });
 
-//Home quotes # todo: values from json or why tf would I yet lul
+// Home quotes # todo: values from json or why tf would I yet lul
 const quoteArray = [
   "Read the instructions",
   "Super duper advanced",
@@ -17,23 +17,23 @@ const quoteArray = [
 ];
 document.getElementById("quote").innerText = quoteArray[Math.floor(Math.random() * quoteArray.length)];
 
-//just a stupid secret
+// Just a stupid secret
 if (Math.random() * 100000 < 69) {
   document.getElementById("quote").innerText = "Wow, you're lucky. only 69 in 100K people get this message :)";
 }
 
-//neat random header height if screen.height > 500px
+// Neat random header height if screen.height > 500px
 if (screen.height > 500) {
   document.getElementById("header").style.height = Math.floor(Math.random() * (45 - 20 + 1) + 20) + "vh";
 }
 
-//fix my jank css || vertically centers the name and description even if it resized
+// Fix my jank css || vertically centers the name and description even if it resized
 const setVerticalHeight = () => {
   document.getElementById("top").style.bottom = document.getElementById("header").offsetHeight / 2 - document.getElementById("top").offsetHeight / 2 + "px";
 };
 setVerticalHeight();
 
-//(when it actually resizes)
+// When it actually resizes
 window.addEventListener("resize", () => {
   setVerticalHeight();
 });
